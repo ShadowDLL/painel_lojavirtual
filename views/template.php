deef49b29c375b9670d02e270afb962e3f882315
orlandocorreia2 <orlandocorreia2@hotmail.com>
2017-01-10 13:47:31 -0200

    Update template.php

--- views/template.php
+++ views/template.php
@@ -30,7 +30,18 @@
                     </ul>
 		</div>		
             </nav>
-                <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+		<?php if (isset($_GET['p']) && !empty($_GET['p']) && $_GET['p'] > 1): ?> 
+                    <a href="/categorias?p=<?php echo ($_GET['p'] - 1); ?>" class="btn btn-default">Anterior</a>
+                <?php endif; ?>    
+                <?php
+                   $page = ceil($quantidade / $limit);
+                   for($i = 1; $i <= $page; $i++): ?>
+                       <a href="/categorias?p=<?php echo $i; ?>" class="btn btn-default"><?php echo $i; ?></a>
+                   <?php endfor;?>
+                 <?php if (isset($_GET['p']) && !empty($_GET['p']) && $_GET['p'] < $page): ?> 
+                     <a href="/categorias?p=<?php echo ($_GET['p'] + 1); ?>" class="btn btn-default">Próxima</a>
+                 <?php endif; ?> 
+                 <?php $this->loadViewInTemplate($viewName, $viewData); ?>
 	</div>
             
             
